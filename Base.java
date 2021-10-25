7801c4efa36a7ba704ff7226c4e2a1707613e0c1
Vikramramwani <89407559+Vikramramwani@users.noreply.github.com>
2021-10-25 20:35:07 +0530

    added a case to call factorial function

--- Base.java
+++ Base.java
@@ -10,8 +10,8 @@ public class Base {
 		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("+-----------------------------------------------+");
-		System.out.println("| 1.Addition \t\t 2.Subtraction \t\t|\n| 3.Multiplication \t 4.Divison \t\t 5.Round Off \t\t|");
-		System.out.println("| 6.Floor \t\t 7.Ceil \t\t|\n| 8.Power(n^m) \t\t 9.Square Root \t|\n| 10.Degree to Radian\t 11.Radian to Degree \t|");
+		System.out.println("| 1.Addition \t\t 2.Subtraction \t\t|\n| 3.Multiplication \t 4.Divison \t\t|\n| 5.Factorial\t\t 6.Round Off \t\t|");
+		System.out.println("| 7.Floor \t\t 8.Ceil \t\t|\n| 9.Power(n^m) \t\t 10.Square Root \t|\n| 11.Degree to Radian\t 12.Radian to Degree \t|");
 		System.out.println("| \t***Press any caracter to EXIT*** \t|");
 		System.out.println("+-----------------------------------------------+");
 
@@ -38,27 +38,33 @@ public class Base {
 					A.div();
 					break;
 	
-				
 				case 5:
+					System.out.print("Enter no to find factorial:- ");
+					A.num=sc.nextInt();
+					A.fact=A.factorial(A.num);
+					System.out.println("Factorial of "+A.num+" is "+A.fact);
+					break;
+				
+				case 6:
 					Cal.round_off();
 					break;
 					
-				case 6:
+				case 7:
 					Cal.floor();
 					break;
 					
-				case 7:
+				case 8:
 					Cal.ceil();
 					break;
 					
-				case 8:
+				case 9:
 					Cal.power();
 					break;
 					
-				case 9:
+				case 10:
 					Cal.Sq_rt();
 					break;
-				case 10:
+				case 11:
 					Cal.deg_to_rad();
 					break;
 				default:
@@ -67,7 +73,7 @@ public class Base {
 			}
 			System.out.print("\n\n*************************");
 			
-		}while(ch<11);
+		}while(ch<12);
 		sc.close();
 
 	}
